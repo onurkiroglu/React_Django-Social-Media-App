af09847217773d6523249c21b8bab21e93c9cd5d
onur8404 <onurkiroglu90@gmail.com>
2021-12-17 00:20:24 +0300

    Our First Template

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,39 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content":x.content} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+def tweet_detail_view(response, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    """
+    
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id = tweet_id)
+        data['content'] = obj.content
+        
+    except: 
+        data['message'] = "Not found"
+        status = 404
+    
+    return JsonResponse(data, status=status)
